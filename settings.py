4e6e19c63da3a4e36bb05c29eff3ea5b5d6e096b
AlexAndronati <alex.andronati@gmail.com>
2020-05-12 19:16:04 +0300

    commit files

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+DB_URL = 'postgresql+psycopg2://postgres:admin@127.0.0.1:5433/covid'
+DB_NAME = 'covid'
+API_URL_ALL = 'https://api.covid19api.com/all'
+API_URL_NEW = 'https://api.covid19api.com/all'
